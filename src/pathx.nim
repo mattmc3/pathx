1c71d714d21e0c938c2473b0f4d5919f78e182f8
mattmc3 <mattmc3@gmail.com>
2025-10-30 11:16:19 -0400

    Update pathx modify command

--- src/pathx.nim
+++ src/pathx.nim
@@ -1,20 +1,25 @@
-import std/[parseopt, os, strutils]
+# https://fishshell.com/docs/current/cmds/path.html
+# https://zsh.sourceforge.io/Doc/Release/Expansion.html#Modifiers
+
+import std/parseopt
+import std/os except normalizePath
+import std/strutils
 
 # Swappable writers export
-type Writer* = proc (s: string)
+type Writer* = proc(s: string)
 
 # Base command providing writer hooks
 type PathxCommand* = ref object of RootObj
   outWriter*: Writer
   errWriter*: Writer
 
-type PathOp = enum
-  Absolute,
-  Parent,
-  Name,
-  Stem,
-  RootStem,
-  Extension,
+type ModifyOp = enum
+  Normalize
+  Dirname
+  Basename
+  Stem
+  RootStem
+  Extension
   Extensions
 
 proc output*(self: PathxCommand, s: string) =
@@ -38,8 +43,8 @@ proc splitAllExts(file: string): (string, seq[string]) =
     let dot = base.rfind('.')
     if dot <= 0: # no dot or leading dot (hidden file) -> stop
       break
-    let ext = base[dot..^1]
-    exts.add(ext)          # first added is the last extension
+    let ext = base[dot ..^ 1]
+    exts.add(ext) # first added is the last extension
     base = base[0 ..< dot]
   (base, exts)
 
@@ -87,28 +92,52 @@ proc allExtsOf(path: string): string =
     chain
 
 proc usage*(self: PathxCommand) =
-  self.output("""
-Usage: pathx [OPTIONS] <paths...>
+  self.output(
+    """
+Usage: pathx <command> [ARGS]
+
+Commands:
+  modify    Apply path transformations
+
+Use 'pathx modify --help' for detailed options.
+"""
+  )
+
+proc modifyUsage(self: PathxCommand) =
+  self.output(
+    """
+Usage: pathx modify [OPTIONS] <paths...>
 Chain short flags to apply successive transformations to each path.
 
 Flags:
-  -a, --absolute      Make path absolute
-  -p, --parent        Replace with parent directory
-  -n, --name          Replace with final path component (file/dir name)
-  -s, --stem          Filename without final extension (filename.tar from filename.tar.gz)
-  -S, --root-stem     Filename without any extensions (filename from filename.tar.gz)
-  -e, --extension     Final extension only (e.g. .gz)
-  -E, --extensions    Full extension chain (e.g. .tar.gz)
+  -n, --normalize     Normalize path (make absolute)
+  -d, --dirname       Replace with parent directory
+  -b, --basename      Replace with final path component (file/dir name)
+  -s, --stem          Filename without final extension (name.tar from name.tar.gz)
+  -S, --root-stem     Filename without any extensions (name from name.tar.gz)
+  -e, --extension     Final extension only (eg: .gz)
+  -E, --extensions    Full extension chain (eg: .tar.gz)
   --help              Show this help
 
 Example:
-  pathx -appn ./foo/../bar/file.tar.gz
-""")
+  pathx modify -nddb ./foo/../bar/file.tar.gz
+"""
+  )
 
-proc pathx*(self: PathxCommand, args: seq[string]): int =
+# Thin wrappers to match op naming while delegating to std/os
+proc normalizePath(path: string): string =
+  absolutePath(path)
+
+proc dirnameOf(path: string): string =
+  parentDir(path)
+
+proc basenameOf(path: string): string =
+  lastPathPart(path)
+
+proc modify*(self: PathxCommand, args: seq[string]): int =
   var positionals: seq[string]
   var optParser = initOptParser(args)
-  var ops: seq[PathOp]
+  var ops: seq[ModifyOp]
 
   for kind, key, val in optParser.getopt():
     case kind
@@ -118,14 +147,14 @@ proc pathx*(self: PathxCommand, args: seq[string]): int =
     of cmdLongOption, cmdShortOption:
       case key
       of "help":
-        self.usage()
+        self.modifyUsage()
         return 0
-      of "a", "absolute":
-        ops.add(Absolute)
-      of "p", "parent":
-        ops.add(Parent)
-      of "n", "name":
-        ops.add(Name)
+      of "n", "normalize":
+        ops.add(Normalize)
+      of "d", "dirname":
+        ops.add(Dirname)
+      of "b", "basename":
+        ops.add(Basename)
       of "s", "stem":
         ops.add(Stem)
       of "S", "root-stem":
@@ -146,19 +175,19 @@ proc pathx*(self: PathxCommand, args: seq[string]): int =
 
   if positionals.len == 0:
     self.outerr("contains: No paths provided.\n")
-    self.usage()
+    self.modifyUsage()
     return 1
 
   for p in positionals:
     var outPath = p
     for op in ops:
       case op
-      of Absolute:
-        outPath = absolutePath(outPath)
-      of Parent:
-        outPath = parentDir(outPath)
-      of Name:
-        outPath = lastPathPart(outPath)
+      of Normalize:
+        outPath = normalizePath(outPath)
+      of Dirname:
+        outPath = dirnameOf(outPath)
+      of Basename:
+        outPath = basenameOf(outPath)
       of Stem:
         outPath = stemOf(outPath)
       of RootStem:
@@ -172,6 +201,25 @@ proc pathx*(self: PathxCommand, args: seq[string]): int =
     self.output(outPath & "\n")
 
   return 0
+
+proc pathx*(self: PathxCommand, args: seq[string]): int =
+  if args.len == 0:
+    self.usage()
+    return 1
+
+  let sub = args[0]
+  case sub
+  of "help", "--help", "-h":
+    self.usage()
+    return 0
+  of "modify":
+    return self.modify(args[1 .. ^1])
+  else:
+    if sub.len > 0 and sub[0] == '-':
+      return self.modify(args)
+    self.outerr("contains: Unknown command '" & sub & "'\n")
+    self.usage()
+    return 2
 
 when isMainModule:
   import os
